d984aaaf868504e5402119ac7d9b979bc2c20045
Rody Davis <rody.davis.jr@gmail.com>
2022-03-01 17:24:36 -0800

    updating

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/html-canvas-utilities/",
   build: {
     rollupOptions: {
-      input: "index.html",
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
     },
   },
 });
